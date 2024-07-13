d186b94fb9da09be48194d044e24762ec19c4935
aswathcm29 <aswathcm29@gmail.com>
2024-07-13 19:00:02 +0530

    handle message

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,22 @@ import handleConnectionTermination from './websocketFunctions/handle-connection-
 import { v4 as uuidv4 } from 'uuid';
 const port = 8080
 const serverId = uuidv4()
+//Used to store the  user and their associated rooms
+let sessionStoreRooms = new Map()
+
 try {
     const wss = new WebSocketServer({ port: port});
+
     wss.on('connection', function connection(ws) {
       ws.on('error', console.error);
+
     ws.on('message', function message(data) {
         console.log('received: %s', data);
       });
+
     ws.send(JSON.stringify({"action":"serverDetails","serverId":serverId+""}))
     });
+    
     console.log("Web socket server started successfully",port)
    } catch (error) {
     console.error("Error while starting the web socket server")
--- backend/websocketFunctions/handle-message.js
+++ backend/websocketFunctions/handle-message.js
@@ -2,7 +2,19 @@ import handleBroadcast from "./handle-broadcast.js"
 import handleJoinRoom from "./handle-join-room.js"
 
 const handleMessage = (data, ws, sessionStoreRooms) => {
-
+    // converting the data from buffer to string
+    const stringifiedData = data .toString()
+    // converting the data from string to object
+    const parsedData = JSON.parse(stringifiedData)
+ 
+ 
+    if(parsedData.action === "joinRoom"){
+        handleJoinRoom(parsedData, ws, sessionStoreRooms)
+    }
+    else if(parsedData.action === "broadcast"){
+        handleBroadcast(parsedData, ws, sessionStoreRooms)
+    }
+ 
 }
 
 export default handleMessage
