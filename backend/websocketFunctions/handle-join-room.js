d7eaf89df69e5c1033d7958e17ed0a45ee39b60a
aswathcm29 <aswathcm29@gmail.com>
2024-07-13 18:55:36 +0530

    Join room Impl

--- backend/websocketFunctions/handle-join-room.js
+++ backend/websocketFunctions/handle-join-room.js
@@ -1,7 +1,27 @@
 import handleBroadcast from "./handle-broadcast.js"
 
 const handleJoinRoom = (parsedData, ws, sessionStoreRooms) => {
- 
+    const {roomId, userName} = parsedData
+
+
+   //Storing the roomId and the userName as a part of the web-socket connection
+   ws.roomId = roomId
+   ws.userName = userName
+
+
+   let roomUsers = sessionStoreRooms.get(roomId)
+
+
+   if(roomUsers){
+       roomUsers.set(userName, ws)
+   }
+   else {
+       const newRoom = new Map()
+       newRoom.set(userName, ws)
+       sessionStoreRooms.set(roomId, newRoom)
+   }
+   //Inform other users in the room about the new user
+   handleBroadcast(parsedData, ws, sessionStoreRooms)
 }
 
 export default handleJoinRoom
